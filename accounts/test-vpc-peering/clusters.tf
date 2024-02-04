3b162bb63d3d4f4c95375bf71baca581e1bc146f
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-02-04 20:05:10 +0100

    Update clusters.tf

--- accounts/test-vpc-peering/clusters.tf
+++ accounts/test-vpc-peering/clusters.tf
@@ -1,8 +1,8 @@
 resource "confluent_kafka_cluster" "dedicated" {
   display_name = "inventory"
   availability = "SINGLE_ZONE"
-  cloud        = confluent_network.peering.cloud
-  region       = confluent_network.peering.region
+  cloud        = "AWS"
+  region       = var.region
   dedicated {
     cku = 1
   }
@@ -12,10 +12,7 @@ resource "confluent_kafka_cluster" "dedicated" {
   network {
     id = confluent_network.peering.id
   }
-  depends_on = [
-    confluent_environment.staging,
-    confluent_network.peering  
-  ]
+
 }
 
 output "confluent_kafka_cluster_id" {
@@ -64,125 +61,5 @@ output "confluent_kafka_cluster_dedicated2_kind" {
   value = confluent_kafka_cluster.dedicated2.kind
 }
 
-resource "confluent_network" "peering" {
-  display_name     = "Peering Network"
-  cloud            = "AWS"
-  region           = var.region
-  cidr             = var.cidr
-  connection_types = ["PEERING"]
-  environment {
-    id = confluent_environment.staging.id
-  }
-depends_on = [
-    confluent_environment.staging  
-  ]
-}
 
 
-resource "confluent_network" "peering3" {
-  display_name     = "Peering Network"
-  cloud            = "AWS"
-  region           = var.region
-  cidr             = var.cidr2
-  connection_types = ["PEERING"]
-  environment {
-    id = confluent_environment.test.id
-  }
-depends_on = [
-    confluent_environment.test  
-  ]
-}
-
-
-
-resource "confluent_peering" "aws" {
-  display_name = "AWS Peering"
-  aws {
-    account         = var.aws_account_id
-    vpc             = var.vpc_id
-    routes          = var.routes
-    customer_region = var.customer_region
-  }
-  environment {
-    id = confluent_environment.staging.id
-  }
-  network {
-    id = confluent_network.peering.id
-  }
-depends_on = [
-    confluent_environment.staging,
-    confluent_network.peering
-  ]
-}
-
-resource "confluent_peering" "aws3" {
-  display_name = "AWS Peering 3"
-  aws {
-    account         = var.aws_account_id
-    vpc             = var.vpc_id
-    routes          = var.routes
-    customer_region = var.customer_region
-  }
-  environment {
-    id = confluent_environment.test.id
-  }
-  network {
-    id = confluent_network.peering3.id
-  }
-depends_on = [
-    confluent_environment.test,
-    confluent_network.peering3
-  ]
-}
-
-# https://docs.confluent.io/cloud/current/networking/peering/aws-peering.html
-# Create a VPC Peering Connection to Confluent Cloud on AWS
-
-provider "aws" {
-  region = var.customer_region
-}
-
-# Accepter's side of the connection.
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/vpc_peering_connection
-
-data "aws_vpc_peering_connection" "accepter" {
-  vpc_id      = confluent_network.peering.aws[0].vpc
-  peer_vpc_id = confluent_peering.aws.aws[0].vpc
-}
-
-data "aws_vpc_peering_connection" "accepter3" {
-  vpc_id      = confluent_network.peering3.aws[0].vpc
-  peer_vpc_id = confluent_peering.aws3.aws[0].vpc
-}
-
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_peering_connection_accepter
-
-resource "aws_vpc_peering_connection_accepter" "peer" {
-  vpc_peering_connection_id = data.aws_vpc_peering_connection.accepter.id
-  auto_accept               = true
-}
-
-resource "aws_vpc_peering_connection_accepter" "peer3" {
-  vpc_peering_connection_id = data.aws_vpc_peering_connection.accepter3.id
-  auto_accept               = true
-}
-
-# Find the routing table
-data "aws_route_tables" "rts" {
-  vpc_id = var.vpc_id
-}
-
-resource "aws_route" "r" {
-  for_each                  = toset(data.aws_route_tables.rts.ids)
-  route_table_id            = each.key
-  destination_cidr_block    = confluent_network.peering.cidr
-  vpc_peering_connection_id = data.aws_vpc_peering_connection.accepter.id
-}
-
-resource "aws_route" "r3" {
-  for_each                  = toset(data.aws_route_tables.rts.ids)
-  route_table_id            = each.key
-  destination_cidr_block    = confluent_network.peering3.cidr
-  vpc_peering_connection_id = data.aws_vpc_peering_connection.accepter3.id
-}
