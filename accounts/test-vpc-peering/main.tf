f62a282e9317c44b717838825e06021d694493df
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-02-04 15:03:52 +0100

    Update main.tf

--- accounts/test-vpc-peering/main.tf
+++ accounts/test-vpc-peering/main.tf
@@ -6,6 +6,14 @@ output "confluent_environment_id" {
   value = confluent_environment.staging.id
 }
 
+resource "confluent_environment" "test" {
+  display_name = "Test"
+}
+
+output "confluent_environment_test_id" {
+  value = confluent_environment.test.id
+}
+
 
 
 // 'app-manager' service account is required in this configuration to create 'orders' topic and grant ACLs
@@ -26,6 +34,23 @@ output "confluent_service_account_app-manager_kind" {
 }
 
 
+
+resource "confluent_service_account" "app-manager2" {
+  display_name = "app-manager2"
+  description  = "Service account to manage 'inventory2' Kafka cluster"
+}
+
+output "confluent_service_account_app-manager2_id" {
+  value = confluent_service_account.app-manager2.id
+}
+output "confluent_service_account_app-manager2_api_version" {
+  value = confluent_service_account.app-manage2r.api_version
+}
+output "confluent_service_account_app-manager2_kind" {
+  value = confluent_service_account.app-manager2.kind
+}
+
+
 resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
   principal   = "User:${confluent_service_account.app-manager.id}"
   role_name   = "CloudClusterAdmin"
@@ -33,6 +58,16 @@ resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
 depends_on = [
     confluent_kafka_cluster.dedicated,
     confluent_service_account.app-manager
+  ]
+}
+
+resource "confluent_role_binding" "app-manager-kafka-cluster-admin2" {
+  principal   = "User:${confluent_service_account.app-manager2.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.dedicated2.rbac_crn
+depends_on = [
+    confluent_kafka_cluster.dedicated2,
+    confluent_service_account.app-manager2
   ]
 }
 
@@ -73,6 +108,43 @@ output "confluent_api_key_secret" {
 }
 
 
+resource "confluent_api_key" "app-manager-kafka-api-key2" {
+  display_name = "app-manager-kafka-api-key2"
+  description  = "Kafka API Key that is owned by 'app-manager2' service account"
+  disable_wait_for_ready = "true"
+  owner {
+    id          = confluent_service_account.app-manager2.id
+    api_version = confluent_service_account.app-manager2.api_version
+    kind        = confluent_service_account.app-manager2.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.dedicated2.id
+    api_version = confluent_kafka_cluster.dedicated2.api_version
+    kind        = confluent_kafka_cluster.dedicated2.kind
+
+    environment {
+      id = confluent_environment.test.id
+    }
+  }
+depends_on = [
+    confluent_kafka_cluster.dedicated2,
+    confluent_environment.test,
+    confluent_service_account.app-manager2  
+  ]
+}
+
+output "confluent_api_key2_id" {
+  value = confluent_api_key.app-manager-kafka-api-key2.id
+}
+
+output "confluent_api_key2_secret" {
+  value = confluent_api_key.app-manager-kafka-api-key2.secret
+  sensitive = true
+}
+
+
+
   # The goal is to ensure that confluent_role_binding.app-manager-kafka-cluster-admin is created before
   # confluent_api_key.app-manager-kafka-api-key is used to create instances of
   # confluent_kafka_topic, confluent_kafka_acl resources.
@@ -98,6 +170,20 @@ depends_on = [
   ]
 }
 
+resource "confluent_network" "peering3" {
+  display_name     = "Peering Network"
+  cloud            = "AWS"
+  region           = var.region
+  cidr             = var.cidr2
+  connection_types = ["PEERING"]
+  environment {
+    id = confluent_environment.test.id
+  }
+depends_on = [
+    confluent_environment.test  
+  ]
+}
+
 
 
 resource "confluent_peering" "aws" {
@@ -117,6 +203,26 @@ resource "confluent_peering" "aws" {
 depends_on = [
     confluent_environment.staging,
     confluent_network.peering
+  ]
+}
+
+resource "confluent_peering" "aws3" {
+  display_name = "AWS Peering 3"
+  aws {
+    account         = var.aws_account_id
+    vpc             = var.vpc_id
+    routes          = var.routes
+    customer_region = var.customer_region
+  }
+  environment {
+    id = confluent_environment.test.id
+  }
+  network {
+    id = confluent_network.peering3.id
+  }
+depends_on = [
+    confluent_environment.test,
+    confluent_network.peering3
   ]
 }
 
@@ -155,6 +261,42 @@ output "confluent_kafka_cluster_kind" {
   value = confluent_kafka_cluster.dedicated.kind
 }
 
+resource "confluent_kafka_cluster" "dedicated2" {
+  display_name = "inventory2"
+  availability = "SINGLE_ZONE"
+  cloud        = confluent_network.peering.cloud
+  region       = confluent_network.peering.region
+  dedicated {
+    cku = 1
+  }
+  environment {
+    id = confluent_environment.test.id
+  }
+  network {
+    id = confluent_network.peering3.id
+  }
+depends_on = [
+    confluent_environment.test,
+    confluent_network.peering2  
+  ]
+}
+
+output "confluent_kafka_cluster2_id" {
+  value = confluent_kafka_cluster2.dedicated.id
+}
+output "confluent_kafka_cluster2_rest_endpoint" {
+  value = confluent_kafka_cluster2.dedicated.rest_endpoint
+}
+output "confluent_kafka_cluster2_api_version" {
+  value = confluent_kafka_cluster2.dedicated.api_version
+}
+output "confluent_kafka_cluster2_kind" {
+  value = confluent_kafka_cluster2.dedicated.kind
+}
+
+
+
+
 
 
 # https://docs.confluent.io/cloud/current/networking/peering/aws-peering.html
@@ -172,11 +314,21 @@ data "aws_vpc_peering_connection" "accepter" {
   peer_vpc_id = confluent_peering.aws.aws[0].vpc
 }
 
+data "aws_vpc_peering_connection" "accepter3" {
+  vpc_id      = confluent_network.peering3.aws[0].vpc
+  peer_vpc_id = confluent_peering.aws3.aws[0].vpc
+}
+
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_peering_connection_accepter
 
 resource "aws_vpc_peering_connection_accepter" "peer" {
   vpc_peering_connection_id = data.aws_vpc_peering_connection.accepter.id
+  auto_accept               = true
+}
+
+resource "aws_vpc_peering_connection_accepter" "peer3" {
+  vpc_peering_connection_id = data.aws_vpc_peering_connection.accepter3.id
   auto_accept               = true
 }
 
@@ -190,5 +342,12 @@ resource "aws_route" "r" {
   route_table_id            = each.key
   destination_cidr_block    = confluent_network.peering.cidr
   vpc_peering_connection_id = data.aws_vpc_peering_connection.accepter.id
+}
+
+resource "aws_route" "r3" {
+  for_each                  = toset(data.aws_route_tables.rts.ids)
+  route_table_id            = each.key
+  destination_cidr_block    = confluent_network.peering3.cidr
+  vpc_peering_connection_id = data.aws_vpc_peering_connection.accepter3.id
 }
 
