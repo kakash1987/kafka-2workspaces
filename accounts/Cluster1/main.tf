51d490c44bee6c27fcd2aec01e3deba0b205d4b1
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-02-04 12:59:40 +0100

    Update main.tf

--- accounts/Cluster1/main.tf
+++ accounts/Cluster1/main.tf
@@ -43,15 +43,15 @@ data "terraform_remote_state" "confluent_kafka_cluster" {
 module "confluent_kafka_topics" {
   source = "../../modules/confluent_kafka_topics_module"
 
-  kafka_id              = data.terraform_remote_state.confluent_kafka_cluster.dedicated.outputs.id
-  kafka_rest_endpoint   = data.terraform_remote_state.confluent_kafka_cluster.dedicated.outputs.rest_endpoint
-  kafka_api_key         = data.terraform_remote_state.confluent_api_key.app-manager-kafka-api-key.outputs.id
-  kafka_api_secret      = data.terraform_remote_state.confluent_api_key.app-manager-kafka-api-key.outputs.secret
+  kafka_id              = data.terraform_remote_state.confluent_kafka_cluster.outputs.id
+  kafka_rest_endpoint   = data.terraform_remote_state.confluent_kafka_cluster.outputs.rest_endpoint
+  kafka_api_key         = data.terraform_remote_state.confluent_api_key.outputs.id
+  kafka_api_secret      = data.terraform_remote_state.confluent_api_key.outputs.secret
   cloud_api_key         = var.confluent_cloud_api_key
   cloud_api_secret      = var.confluent_cloud_api_secret
-  environment_id        = data.terraform_remote_state.confluent_environment.staging.outputs.id
-  kafka_api_version     = data.terraform_remote_state.confluent_kafka_cluster.dedicated.outputs.api_version
-  kafka_kind            = data.terraform_remote_state.confluent_kafka_cluster.dedicated.outputs.kind
+  environment_id        = data.terraform_remote_state.confluent_environment.outputs.id
+  kafka_api_version     = data.terraform_remote_state.confluent_kafka_cluster.outputs.api_version
+  kafka_kind            = data.terraform_remote_state.confluent_kafka_cluster.outputs.kind
   confluent_service_account_id          = data.terraform_remote_state.confluent_service_account.app-manager.outputs.id
   confluent_service_account_api_version = data.terraform_remote_state.confluent_service_account.app-manager.outputs.api_version
   confluent_service_account_kind        = data.terraform_remote_state.confluent_service_account.app-manager.outputs.kind
