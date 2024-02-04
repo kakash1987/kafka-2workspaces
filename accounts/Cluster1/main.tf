e1c56489a470f7e34251ea717f6cd320ad0807ed
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-02-04 13:38:13 +0100

    Update main.tf

--- accounts/Cluster1/main.tf
+++ accounts/Cluster1/main.tf
@@ -44,17 +44,17 @@ module "confluent_kafka_topics" {
   source = "../../modules/confluent_kafka_topics_module"
 
   kafka_id              = data.terraform_remote_state.confluent_kafka_cluster.outputs.confluent_kafka_cluster_id
-  kafka_rest_endpoint   = data.terraform_remote_state.confluent_kafka_cluster.outputs.rest_endpoint
-  kafka_api_key         = data.terraform_remote_state.confluent_api_key.outputs.id
-  kafka_api_secret      = data.terraform_remote_state.confluent_api_key.outputs.secret
+  kafka_rest_endpoint   = data.terraform_remote_state.confluent_kafka_cluster.outputs.confluent_kafka_cluster_rest_endpoint
+  kafka_api_key         = data.terraform_remote_state.confluent_api_key.outputs.confluent_api_key_id
+  kafka_api_secret      = data.terraform_remote_state.confluent_api_key.outputs.confluent_api_key_secret
   cloud_api_key         = var.confluent_cloud_api_key
   cloud_api_secret      = var.confluent_cloud_api_secret
-  environment_id        = data.terraform_remote_state.confluent_environment.outputs.id
-  kafka_api_version     = data.terraform_remote_state.confluent_kafka_cluster.outputs.api_version
-  kafka_kind            = data.terraform_remote_state.confluent_kafka_cluster.outputs.kind
-  confluent_service_account_id          = data.terraform_remote_state.confluent_service_account.outputs.id
-  confluent_service_account_api_version = data.terraform_remote_state.confluent_service_account.outputs.api_version
-  confluent_service_account_kind        = data.terraform_remote_state.confluent_service_account.outputs.kind
+  environment_id        = data.terraform_remote_state.confluent_environment.outputs.confluent_environment_id
+  kafka_api_version     = data.terraform_remote_state.confluent_kafka_cluster.outputs.confluent_kafka_cluster_api_version
+  kafka_kind            = data.terraform_remote_state.confluent_kafka_cluster.outputs.confluent_kafka_cluster_kind
+  confluent_service_account_id          = data.terraform_remote_state.confluent_service_account.outputs.confluent_service_account_app-manager_id
+  confluent_service_account_api_version = data.terraform_remote_state.confluent_service_account.outputs.confluent_service_account_app-manager_api_version
+  confluent_service_account_kind        = data.terraform_remote_state.confluent_service_account.outputs.confluent_service_account_app-manager_kind
   topics                = jsondecode(file("topics.json"))
 
 }
