7d5f571d65e6df840754f1ded806ddf0f00522cb
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-02-05 23:17:27 +0100

    Update main.tf

--- modules/confluent-aws-peering/main.tf
+++ modules/confluent-aws-peering/main.tf
@@ -11,12 +11,12 @@ resource "confluent_peering" "aws" {
     #id = confluent_environment.staging.id
   }
   network {
-    id = var.confluent_network_aws_peering_id
+    id = var.confluent_network_peering_id
     #id = confluent_network.peering.id
   }
   depends_on = [
     var.confluent_environment_id,
-    var.confluent_network_aws_peering_id
+    var.confluent_network_peering_id
     #confluent_environment.staging,
     #confluent_network.peering
   ]
@@ -27,7 +27,7 @@ provider "aws" {
 }
 
 data "aws_vpc_peering_connection" "accepter" {
-  vpc_id      = var.confluent_network_aws_peering_vpc
+  vpc_id      = var.confluent_network_peering_vpc
   #vpc_id      = confluent_network.peering.aws[0].vpc
   peer_vpc_id = confluent_peering.aws.aws[0].vpc
 }
@@ -45,7 +45,7 @@ resource "aws_route" "r" {
   for_each  = toset(data.aws_route_tables.rts.ids)
 
   route_table_id            = each.key
-  destination_cidr_block    = var.confluent_network_aws_peering_cidr
+  destination_cidr_block    = var.confluent_network_peering_cidr
   #destination_cidr_block    = confluent_network.peering.cidr
   vpc_peering_connection_id = data.aws_vpc_peering_connection.accepter.id
 }
