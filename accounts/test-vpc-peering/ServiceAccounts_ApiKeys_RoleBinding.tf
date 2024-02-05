6aa381cb4b6c34ee25b002d70f3733723af896f2
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-02-05 19:18:24 +0100

    Update ServiceAccounts_ApiKeys_RoleBinding.tf

--- accounts/test-vpc-peering/ServiceAccounts_ApiKeys_RoleBinding.tf
+++ accounts/test-vpc-peering/ServiceAccounts_ApiKeys_RoleBinding.tf
@@ -15,20 +15,20 @@ output "confluent_service_account_app-manager_kind" {
 
 
 
-resource "confluent_service_account" "app-manager2" {
-  display_name = "app-manager2"
-  description  = "Service account to manage 'inventory2' Kafka cluster"
-}
+#resource "confluent_service_account" "app-manager2" {
+#  display_name = "app-manager2"
+#  description  = "Service account to manage 'inventory2' Kafka cluster"
+#}
 
-output "confluent_service_account_app-manager2_id" {
-  value = confluent_service_account.app-manager2.id
-}
-output "confluent_service_account_app-manager2_api_version" {
-  value = confluent_service_account.app-manager2.api_version
-}
-output "confluent_service_account_app-manager2_kind" {
-  value = confluent_service_account.app-manager2.kind
-}
+#output "confluent_service_account_app-manager2_id" {
+#  value = confluent_service_account.app-manager2.id
+#}
+#output "confluent_service_account_app-manager2_api_version" {
+#  value = confluent_service_account.app-manager2.api_version
+#}
+#output "confluent_service_account_app-manager2_kind" {
+#  value = confluent_service_account.app-manager2.kind
+#}
 
 resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
   principal   = "User:${confluent_service_account.app-manager.id}"
@@ -40,15 +40,15 @@ depends_on = [
   ]
 }
 
-resource "confluent_role_binding" "app-manager-kafka-cluster-admin2" {
-  principal   = "User:${confluent_service_account.app-manager2.id}"
-  role_name   = "CloudClusterAdmin"
-  crn_pattern = confluent_kafka_cluster.dedicated2.rbac_crn
-depends_on = [
-    confluent_kafka_cluster.dedicated2,
-    confluent_service_account.app-manager2
-  ]
-}
+#resource "confluent_role_binding" "app-manager-kafka-cluster-admin2" {
+#  principal   = "User:${confluent_service_account.app-manager2.id}"
+#  role_name   = "CloudClusterAdmin"
+#  crn_pattern = confluent_kafka_cluster.dedicated2.rbac_crn
+#depends_on = [
+#    confluent_kafka_cluster.dedicated2,
+#    confluent_service_account.app-manager2
+#  ]
+#}
 
 
 resource "confluent_api_key" "app-manager-kafka-api-key" {
@@ -87,37 +87,37 @@ output "confluent_api_key_secret" {
 }
 
 
-resource "confluent_api_key" "app-manager-kafka-api-key2" {
-  display_name = "app-manager-kafka-api-key2"
-  description  = "Kafka API Key that is owned by 'app-manager2' service account"
-  disable_wait_for_ready = "true"
-  owner {
-    id          = confluent_service_account.app-manager2.id
-    api_version = confluent_service_account.app-manager2.api_version
-    kind        = confluent_service_account.app-manager2.kind
-  }
+#resource "confluent_api_key" "app-manager-kafka-api-key2" {
+#  display_name = "app-manager-kafka-api-key2"
+#  description  = "Kafka API Key that is owned by 'app-manager2' service account"
+#  disable_wait_for_ready = "true"
+#  owner {
+#    id          = confluent_service_account.app-manager2.id
+#    api_version = confluent_service_account.app-manager2.api_version
+#    kind        = confluent_service_account.app-manager2.kind
+#  }
 
-  managed_resource {
-    id          = confluent_kafka_cluster.dedicated2.id
-    api_version = confluent_kafka_cluster.dedicated2.api_version
-    kind        = confluent_kafka_cluster.dedicated2.kind
+#  managed_resource {
+#    id          = confluent_kafka_cluster.dedicated2.id
+#    api_version = confluent_kafka_cluster.dedicated2.api_version
+#    kind        = confluent_kafka_cluster.dedicated2.kind
 
-    environment {
-      id = confluent_environment.test.id
-    }
-  }
-depends_on = [
-    confluent_kafka_cluster.dedicated2,
-    confluent_environment.test,
-    confluent_service_account.app-manager2  
-  ]
-}
+#    environment {
+#      id = confluent_environment.test.id
+#    }
+#  }
+#depends_on = [
+#    confluent_kafka_cluster.dedicated2,
+#    confluent_environment.test,
+#    confluent_service_account.app-manager2  
+#  ]
+#}
 
-output "confluent_api_key2_id" {
-  value = confluent_api_key.app-manager-kafka-api-key2.id
-}
+#output "confluent_api_key2_id" {
+#  value = confluent_api_key.app-manager-kafka-api-key2.id
+#}
 
-output "confluent_api_key2_secret" {
-  value = confluent_api_key.app-manager-kafka-api-key2.secret
-  sensitive = true
-}
+#output "confluent_api_key2_secret" {
+#  value = confluent_api_key.app-manager-kafka-api-key2.secret
+#  sensitive = true
+#}
