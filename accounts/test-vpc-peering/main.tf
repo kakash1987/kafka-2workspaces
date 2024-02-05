9688e5663ad265541677711f6f69a20f320b7ccb
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-02-05 23:24:27 +0100

    Update main.tf

--- accounts/test-vpc-peering/main.tf
+++ accounts/test-vpc-peering/main.tf
@@ -170,19 +170,19 @@ depends_on = [
   ]
 }
 
-#resource "confluent_network" "peering3" {
-#  display_name     = "Peering Network"
-#  cloud            = "AWS"
-#  region           = var.region
-#  cidr             = var.cidr2
-#  connection_types = ["PEERING"]
-#  environment {
-#    id = confluent_environment.test.id
-#  }
-#depends_on = [
-#    confluent_environment.test  
-#  ]
-#}
+resource "confluent_network" "peering3" {
+  display_name     = "Peering Network"
+  cloud            = "AWS"
+  region           = var.region
+  cidr             = var.cidr2
+  connection_types = ["PEERING"]
+  environment {
+    id = confluent_environment.test.id
+  }
+depends_on = [
+    confluent_environment.test  
+  ]
+}
 
 
 
