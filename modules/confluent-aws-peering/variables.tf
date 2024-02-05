4640fab0d09f3e29586fd6c1fef75f888f844fd0
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-02-05 21:16:14 +0100

    Create variables.tf

---
+++ modules/confluent-aws-peering/variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  description = "AWS region where resources need to be created"
+  type    = string
+}
+
+variable "vpc_id" {
+  description = "The AWS VPC ID of the peer VPC that you're peering with Confluent Cloud."
+  type        = string
+}
+
+variable "routes" {
+  description = "The AWS VPC CIDR blocks or subsets. This must be from the supported CIDR blocks and must not overlap with your Confluent Cloud CIDR block or any other network peering connection VPC CIDR."
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "confluent_environment_id" {
+  type = string
+}
+
+variable "confluent_network_aws_peering_id" {
+  type = string
+}
+
+variable "confluent_network_aws_peering_vpc" {
+  type = string
+}
+
+variable "confluent_network_aws_peering_cidr" {
+  type = string
+}
