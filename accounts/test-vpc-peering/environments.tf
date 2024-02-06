e163efb9d72abba951da0537d409a6f1d96b411d
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-02-06 08:34:20 +0100

    Update environments.tf

--- accounts/test-vpc-peering/environments.tf
+++ accounts/test-vpc-peering/environments.tf
@@ -6,10 +6,4 @@ output "confluent_environment_id" {
   value = confluent_environment.staging.id
 }
 
-resource "confluent_environment" "test" {
-  display_name = "Test"
-}
 
-output "confluent_environment_test_id" {
-  value = confluent_environment.test.id
-}
