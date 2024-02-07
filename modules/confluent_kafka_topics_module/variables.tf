443632c5ef9489753bc8b03ef5f9409974df39a0
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-02-07 20:49:55 +0100

    Update variables.tf

--- modules/confluent_kafka_topics_module/variables.tf
+++ modules/confluent_kafka_topics_module/variables.tf
@@ -1,16 +1,16 @@
-variable "cloud_api_key" {
-  description = "Confluent Cloud API Key (also referred as Cloud API ID)"
-  type        = string
-  sensitive   = true
-  default = "PRDI5NKO3K2XGAJJ"
-}
+#variable "cloud_api_key" {
+#  description = "Confluent Cloud API Key (also referred as Cloud API ID)"
+#  type        = string
+#  sensitive   = true
+#  default = "PRDI5NKO3K2XGAJJ"
+#}
 
-variable "cloud_api_secret" {
-  description = "Confluent Cloud API Secret"
-  type        = string
-  sensitive   = true
-  default = "4Dtmgmat7Dw98AI0wK7Sz6WRYDTLevQY8HKxPokZknCtN6pu+tRnbRm8rZezy4rQ"
-}
+#variable "cloud_api_secret" {
+#  description = "Confluent Cloud API Secret"
+#  type        = string
+#  sensitive   = true
+#  default = "4Dtmgmat7Dw98AI0wK7Sz6WRYDTLevQY8HKxPokZknCtN6pu+tRnbRm8rZezy4rQ"
+#}
 
 
 variable "confluent_cloud_api_key" {
