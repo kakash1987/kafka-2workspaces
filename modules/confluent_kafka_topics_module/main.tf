d8b5808d529a97d75b137c56434e76a2d58c8f5b
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-02-07 20:01:09 +0100

    Update main.tf

--- modules/confluent_kafka_topics_module/main.tf
+++ modules/confluent_kafka_topics_module/main.tf
@@ -7,17 +7,6 @@ terraform {
   }
 }
 
-#provider "confluent" {
-  #cloud_api_key    = var.confluent_cloud_api_key    # optionally use CONFLUENT_CLOUD_API_KEY env var
-  #cloud_api_secret = var.confluent_cloud_api_secret # optionally use CONFLUENT_CLOUD_API_SECRET env var
-  #kafka_id            = var.kafka_id
-  #kafka_rest_endpoint = var.kafka_rest_endpoint
-  #kafka_api_key       = var.kafka_api_key
-  #kafka_api_secret    = var.kafka_api_secret
-#}
-
-
-
 
 resource "confluent_kafka_topic" "main" {
   for_each = var.topics
