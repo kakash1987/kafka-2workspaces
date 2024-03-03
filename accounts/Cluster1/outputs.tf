c83507f19370dc5f3f932d112e2b8970f65787fa
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-03-03 18:25:43 +0100

    Update outputs.tf

--- accounts/Cluster1/outputs.tf
+++ accounts/Cluster1/outputs.tf
@@ -4,13 +4,11 @@ output "resource-ids" {
 Kafka topic name: ${var.topic_name}
 
   Kafka API Key to consume the data from the '${var.topic_name}' topic:
-  Kafka API Key:     "${module.confluent_kafka_topics_module.consumer_kafka_api_key_id}"
-  Kafka API Secret:  "${module.confluent_kafka_topics_module.consumer_kafka_api_key_secret}"
+  
+  
 
   Kafka API Key to produce the data to the '${var.topic_name}' topic:
-  Kafka API Key:     "${module.confluent_kafka_topics_module.producer_kafka_api_key_id}"
-  Kafka API Secret:  "${module.confluent_kafka_topics_module.producer_kafka_api_key_secret}"
-
+  
 EOT
 
   sensitive = true
