e81dfc05e9433f3dd0c719231a04289dc817a29b
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-03-03 21:40:44 +0100

    Update outputs.tf

--- modules/confluent_kafka_topics_module/outputs.tf
+++ modules/confluent_kafka_topics_module/outputs.tf
@@ -1,13 +1,6 @@
-output "resource-ids" {
-  value = <<-EOT
-
 output "kafka_topics" {
   description = "Map of created Confluent Kafka topics"
-  value       = nonsensitive(confluent_kafka_topic.main)
+  value       = confluent_kafka_topic.main
 }
 
-EOT
-
-  sensitive = true
-}
 
