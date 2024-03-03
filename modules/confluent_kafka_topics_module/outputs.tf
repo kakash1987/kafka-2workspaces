a5575272b255364702634f54a395cbe178b51282
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-03-03 22:03:26 +0100

    Update outputs.tf

--- modules/confluent_kafka_topics_module/outputs.tf
+++ modules/confluent_kafka_topics_module/outputs.tf
@@ -1,16 +1,16 @@
-output "kafka_topics" {
-  description = "Map of created Confluent Kafka topics"
-  value       = confluent_kafka_topic.main
-}
+#output "kafka_topics" {
+#  description = "Map of created Confluent Kafka topics"
+#  value       = confluent_kafka_topic.main
+#}
 
 output "service_accounts_consumers" {
   description = "Map of created SA for consumers"
-  value       = confluent_service_account.app-consumer
+  value       = values(confluent_service_account.app-consumer).*.id
 }
 
-output "confluent_api_key" {
-  description = "Map of created API keys for consumers"
-  value       = confluent_api_key.app-producer-kafka-api-key.id
-}
+#output "confluent_api_key" {
+#  description = "Map of created API keys for consumers"
+#  value       = confluent_api_key.app-producer-kafka-api-key.id
+#}
 
 
