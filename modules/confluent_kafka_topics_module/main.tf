9a09c3645a304916f6eb2026988e8ce939db3633
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-03-03 19:54:06 +0100

    Update main.tf

--- modules/confluent_kafka_topics_module/main.tf
+++ modules/confluent_kafka_topics_module/main.tf
@@ -35,11 +35,11 @@ resource "confluent_kafka_topic" "main" {
 #}
 
 output "first_topic_name" {
-  value = confluent_kafka_topic.main.[0].topic_name
+  value = confluent_kafka_topic.main.topic_name[0]
 }
 
 output "second_topic_name" {
-  value = confluent_kafka_topic.main.[1].topic_name
+  value = confluent_kafka_topic.main.topic_name[1]
 }
 
 
