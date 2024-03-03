55b39cb1fd0b90ebd46b4cc8ff201b8b67b91005
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-03-03 22:17:31 +0100

    Update outputs.tf

--- accounts/Cluster1/outputs.tf
+++ accounts/Cluster1/outputs.tf
@@ -4,15 +4,21 @@
 #  sensitive = true
 #}
 
-output "created_service_accounts_consumers" {
-  description = "Map of created SA for consumers"
-  value       = module.confluent_kafka_topics.service_accounts_consumers[*].id
-}
+#output "created_service_accounts_consumers" {
+#  description = "Map of created SA for consumers"
+#  value       = module.confluent_kafka_topics.service_accounts_consumers[*].id
+#}
 
 #output "created_confluent_api_key" {
 #  description = "Map of created API keys for consumers"
 #  value       = module.confluent_kafka_topics.confluent_api_key.id
 #}
+
+
+
+output "created_target-groups-arn-alternatice" {
+  value =  module.confluent_kafka_topics.target-groups-arn-alternatice
+}
 
 
 
