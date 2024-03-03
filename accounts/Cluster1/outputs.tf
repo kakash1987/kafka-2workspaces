3225530b84efd0675d3cd19a131fb76c679ed34f
kakash1987 <102479856+kakash1987@users.noreply.github.com>
2024-03-03 21:25:46 +0100

    Update outputs.tf

--- accounts/Cluster1/outputs.tf
+++ accounts/Cluster1/outputs.tf
@@ -1,6 +1,5 @@
 output "created_kafka_topics" {
   description = "Map of created Confluent Kafka topics"
-  value       = module.confluent_kafka_topics.kafka_topics
-  sensitive = false
+  value       = nonsensitive(module.confluent_kafka_topics.kafka_topics)
 }
 
